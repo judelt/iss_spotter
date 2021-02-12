48e29cea6efff1bb525e227a10f5d97ce6397736
Judit Mendez Puig <mendez.puig@gmail.com>
2021-02-12 03:50:04 +0000

    linted

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { nextISSTimesForMyLocation } = require('./iss');
 
-/** 
- * Input: 
+/**
+ * Input:
  *   Array of data objects defining the next fly-overs of the ISS.
  *   [ { risetime: <number>, duration: <number> }, ... ]
- * Returns: 
+ * Returns:
  *   undefined
- * Sideffect: 
+ * Sideffect:
  *   Console log messages to make that data more human readable.
  *   Example output:
  *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
